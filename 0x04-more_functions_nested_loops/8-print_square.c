978a17a1c4c789940e8c2862f76c83ba48662ffd
my <my@gmail.com>
2023-03-26 03:46:24 -0700

    mm

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,6 +9,7 @@ void print_square(int size)
 if (size <= 0)
 {
 _putchar('\n');
+}
  else
 {
 int i, j;
@@ -16,7 +17,7 @@ int i, j;
 for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
-			{
+{
 _putchar('#');
 }
 _putchar('\n');
